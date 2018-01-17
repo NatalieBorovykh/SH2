b093b6e7dd1b0cb7f80648d7d844c6be62073adc
Natalie.Borovykh <natalie.borovykh@smartbear.com>
2018-01-17 16:21:52 +0000

    Generated code by SwaggerHub

---
+++ swagger/src/main/java/io/swagger/api/factories/UserApiServiceFactory.java
@@ -0,0 +1,13 @@
+package io.swagger.api.factories;
+
+import io.swagger.api.UserApiService;
+import io.swagger.api.impl.UserApiServiceImpl;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2018-01-17T16:21:52.686Z")
+public class UserApiServiceFactory {
+    private final static UserApiService service = new UserApiServiceImpl();
+
+    public static UserApiService getUserApi() {
+        return service;
+    }
+}
